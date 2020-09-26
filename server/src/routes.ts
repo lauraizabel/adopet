f531fcbf13b3469154b0336b38b5430cf95e5647
Laura Izabel <lauraizabel2563@gmail.com>
2020-09-25 22:22:29 -0300

    auth routes + middlewares

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import PetsControllers from "./controllers/PetsControllers";
 import multer from "multer";
 import multerConfig from "./config/multer";
 import SessionsController from "./controllers/SessionsController";
+import ensureAuthenticated from "./middleware/ensureAuthenticated";
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -13,12 +14,19 @@ const petController = new PetsControllers();
 
 routes.get("/users", userController.show); //ok
 routes.post("/users", userController.create); //ok
-routes.delete("/users/:id", userController.delete); //ok
+routes.delete("/users/:id", ensureAuthenticated, userController.delete); //ok
 
 routes.get("/pets", petController.show); //ok
 routes.get("/pets/:id", petController.index); //ok
-routes.post("/pets/create", upload.single("image"), petController.create); //ok
-routes.delete("/pets/:id", petController.delete); //ok
+
+routes.post(
+  "/pets/create",
+  ensureAuthenticated,
+  upload.single("image"),
+  petController.create
+); //ok
+
+routes.delete("/pets/:id", ensureAuthenticated, petController.delete); //ok
 
 routes.post("/auth", SessionsController.auth);
 
